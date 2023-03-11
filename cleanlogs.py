eecdefe82dfbcd551a005c91483ec7c5d494373c
Karim <37943746+ksaadDE@users.noreply.github.com>
2023-03-11 01:44:11 +0100

    added operationsAmount, refactoring

--- cleanlogs.py
+++ cleanlogs.py
@@ -2,35 +2,41 @@
 # created_at: 03.11.2023
 # Truncates Log Files to size 0 in /var/log, /tmp and /var/lib/docker/containers
 # returns metric message
-
 import logging
 import sys
+import os
+
 from operator import add
+from pathlib import Path
 
 logging.basicConfig(stream=sys.stdout, level=logging.ERROR)
-
-from pathlib import Path
 
 dockerContainerPath="/var/lib/docker/containers"
 sysLogPath="/var/log"
 tmpLogPath="/tmp"
 
-def cleanLogsInPath(p):
+def cleanLogsInPath(p, selector="*.log"):
     amount=0
     cleanedAmount=0
     failedAmount=0
-    for path in Path(p).rglob('*.log'):
+    for path in Path(p).rglob(selector):
+        fpath=path.resolve()
+
+        if not os.path.isfile(path):
+            continue
+
         amount+=1
+
         try:
-            fpath=path.resolve()
             with open(fpath, 'r+') as f:
-            f.truncate(0)
-            logging.info("[+] truncated logfile '{}'".format(fpath))
-            cleanedAmount+=1
+                f.truncate(0)
+                logging.info("[+] truncated logfile '{}'".format(fpath))
+                cleanedAmount+=1
         except Exception as e:
-            logging.exception(e)
-            print("[-] ERROR".format(e))
-            failedAmount+=1
+                logging.exception(e)
+                print("[-] ERROR".format(e))
+                failedAmount+=1
+    
     return [amount, cleanedAmount, failedAmount]
 
 def calcSums (ar=[]):
@@ -43,16 +49,20 @@ def calcSums (ar=[]):
     return previous
 
 
-amount1 = cleanLogsInPath (dockerContainerPath)
-amount2 = cleanLogsInPath (sysLogPath)
-amount3 = cleanLogsInPath (tmpLogPath)
+amounts = [
+    cleanLogsInPath (dockerContainerPath),
+    cleanLogsInPath (sysLogPath, selector="*"),
+    cleanLogsInPath (tmpLogPath)
+]
 
-sums = calcSums([amount1, amount2, amount3])
+sums = calcSums(amounts)
 
 totalFiles=sums[0]
 totalCleaned=sums[1]
 totalFailed=sums[2]
 
+totalOperations = len(amounts)
+
 percentageSuccess = round((totalCleaned/totalFiles), 2)*100
 
-print("[+] Truncated Log Files. [total: {}, success: {}, failed: {}, percent:{:.2f}]".format(totalFiles, totalCleaned, totalFailed, percentageSuccess))
+print("[+] Truncated Log Files. [operations: {}, total: {}, success: {}, failed: {}, percent:{:.2f}]".format(totalOperations, totalFiles, totalCleaned, totalFailed, percentageSuccess))
